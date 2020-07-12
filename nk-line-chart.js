e87d464e3c0b92fcda3130c9cccabb9844472b23
Naoaki Yamada <n-yamada@s-cradle.com>
2020-07-12 11:01:00 +0900

    refactor(chart): Rewrite with PolymerElement

--- nk-line-chart.js
+++ nk-line-chart.js
@@ -4,314 +4,319 @@ Copyright 2016-2020, Naoaki Yamada
 All rights reserved.
 This source code is distributed under the MIT license.
 */
-import '@polymer/polymer/polymer-legacy.js';
-
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { html, PolymerElement } from '@polymer/polymer';
+import { LegacyElementMixin } from '@polymer/polymer/lib/legacy/legacy-element-mixin';
 import { camelToDashCase } from '@polymer/polymer/lib/utils/case-map.js';
 
-var BASE_CLASS = 'nk-line-chart';
-var NON_BUBBLES = {bubbles: false};
-var NS = 'http://www.w3.org/2000/svg';
+const BASE_CLASS = 'nk-line-chart';
+const NON_BUBBLES = {bubbles: false};
+const NS = 'http://www.w3.org/2000/svg';
 
-Polymer({
-  _template: html`
-    <style>
-      :host {
-        display: block;
-      }
-      #container {
-        width: 100%;
-        height: 100%;
-        @apply --nk-line-chart-container;
-      }
-    </style>
-    <div id="container"></div>
-`,
+export class NkLineChartElement extends LegacyElementMixin(PolymerElement) {
+  static get is() {
+    return 'nk-line-chart';
+  }
 
-  is: 'nk-line-chart',
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+        #container {
+          width: 100%;
+          height: 100%;
+          @apply --nk-line-chart-container;
+        }
+      </style>
+      <div id="container"></div>
+    `
+  }
 
-  properties: {
-
-    /**
-     * Style for the background rect of the chart area
-     */
-    backgroundRectStyle: {
-      type: Object,
-      value: function() {
-        return {
-          fill: 'none',
-          stroke: 'none'
-        };
-      }
-    },
-
-    /**
-     * An object to configure the placement and size of the chart area.
-     *
-     * The following members are required.
-     * `top`, `left`, `width`, `height`
-     * Currently, the supported format is only a number of pixels.
-     *
-     * Example:
-     * ```
-     * {
-     *   top: 10,
-     *   left: 10,
-     *   width: 320,
-     *   height: 100
-     * }
-     * ```
-     */
-    chartArea: {
-      type: Object
-    },
-
-    /**
-     * The origin of coordinates. The possible values are,
-     *
-     * ```
-     * left-bottom: cartesian cordinates
-     * left-top: screen cordinates
-     * right-top
-     * right-bottom
-     * ```
-     */
-    origin: {
-      type: String,
-      value: 'left-bottom'
-    },
-
-    /**
-     * Style for polygonal lines
-     */
-    pathStyle: {
-      type: Object,
-      value: function() {
-        return {
-          fill: 'none',
-          stroke: '#e08080',
-          strokeWidth: 2,
-        };
-      }
-    },
-
-    /**
-     * Option for labels at points
-     *
-     * ```
-     * enable: whether show labels or not (boolean)
-     * textFormat: function for label formatter (function(row, index))
-     * offset: offset from label ({x, y})
-     * textStyle: text styles (object)
-     * ```
-     */
-    pointLabel: {
-      type: Object,
-      value: function() {
-        return {
-          enable: false,
-          offset: {
-            y: -10
-          },
-          textFormat: formatLabel,
-          textStyle: {
-            fill: '#333',
-            textAnchor: 'middle'
-          }
-        };
-      }
-    },
-
-    /**
-     * Option for style of data points.
-     *
-     * ```
-     * enable: If false, this option will be ignored.
-     * elements: Function or array of elements which define styles of data points. Currently, `circle` is the only permittable type.
-     *
-     * (ex) A point which consists of two different circles.
-     * elements: [
-     *   {
-     *     type: 'circle',
-     *     style: {
-     *       fill: '#fff',
-     *       r: '8',
-     *       stroke: '#e08080',
-     *       strokeWidth: 1.5
-     *     }
-     *   },
-     *   {
-     *     type: 'circle',
-     *     style: {
-     *       fill: '#e08080',
-     *       r: '3.5',
-     *       stroke: 'none',
-     *       strokeWidth: 0
-     *     }
-     *    }
-     *  ]
-     *
-     * (ex) Can be a function which returns an array of elements
-     * elements: function(p, i) {
-     *   if (i === this._someIndex) {
-     *     return [
-     *       {
-     *         type: 'circle',
-     *         style: {
-     *           fill: 'red',
-     *           r: '8'
-     *         }
-     *       }
-     *     ];
-     *   }
-     *   else {
-     *     return [
-     *       {
-     *         type: 'circle',
-     *         style: {
-     *           fill: 'blue',
-     *           r: '6'
-     *         }
-     *       }
-     *     ];
-     *   }
-     * }.bind(this)
-     * ```
-     */
-    point: {
-      type: Object,
-      value: function() {
-        return {
-          enable: true,
-          elements: [
-            {
-              type: 'circle',
-              style: {
-                fill: '#e08080',
-                r: '3',
-                stroke: 'none',
-                strokeWidth: 2
-              }
-            }
-          ]
-        };
+  static get properties() {
+    return {
+      /**
+       * Style for the background rect of the chart area
+       */
+      backgroundRectStyle: {
+        type: Object,
+        value: function() {
+          return {
+            fill: 'none',
+            stroke: 'none'
+          };
+        }
       },
-    },
 
-    /**
-     * Data rows
-     *
-     * ```
-     * (ex)
-     * [
-     *   [0, 0], [1, 1]
-     * ]
-     * ```
-     */
-    rows: {
-      type: Array
-    },
+      /**
+       * An object to configure the placement and size of the chart area.
+       *
+       * The following members are required.
+       * `top`, `left`, `width`, `height`
+       * Currently, the supported format is only a number of pixels.
+       *
+       * Example:
+       * ```
+       * {
+       *   top: 10,
+       *   left: 10,
+       *   width: 320,
+       *   height: 100
+       * }
+       * ```
+       */
+      chartArea: {
+        type: Object
+      },
 
-    /**
-     * Option for x-axis grid lines and labels
-     */
-    xAxis: {
-      type: Object,
-      value: function() {
-        return {
-          enable: false,
-          label: {
-            offset: {
-              x: -4,
-              y: 5
-            },
-            textStyle: {
-              fill: '#333',
-              textAnchor: 'end'
-            }
-          },
-          lineStyle: {
+      /**
+       * The origin of coordinates. The possible values are,
+       *
+       * ```
+       * left-bottom: cartesian cordinates
+       * left-top: screen cordinates
+       * right-top
+       * right-bottom
+       * ```
+       */
+      origin: {
+        type: String,
+        value: 'left-bottom'
+      },
+
+      /**
+       * Style for polygonal lines
+       */
+      pathStyle: {
+        type: Object,
+        value: function() {
+          return {
             fill: 'none',
             stroke: '#e08080',
-            strokeDasharray: '4,2',
-            strokeWidth: 1
-          },
-          tickInterval: 2
+            strokeWidth: 2,
+          };
         }
-      }
-    },
+      },
 
-    /**
-     * X-axis range of chart area. If not specified, the range will be computed by data rows.
-     *
-     * ```
-     * min: X-axis value at the left (right if the origin is right) of chart area
-     * max: X-axis value at the right (left if the origin is right) of chart area
-     * ```
-     */
-    xRange: {
-      type: Object,
-      value: null
-    },
-
-    /**
-     * Option for y-axis grid lines and labels
-     */
-    yAxis: {
-      type: Object,
-      value: function() {
-        return {
-          enable: false,
-          label: {
+      /**
+       * Option for labels at points
+       *
+       * ```
+       * enable: whether show labels or not (boolean)
+       * textFormat: function for label formatter (function(row, index))
+       * offset: offset from label ({x, y})
+       * textStyle: text styles (object)
+       * ```
+       */
+      pointLabel: {
+        type: Object,
+        value: function() {
+          return {
+            enable: false,
             offset: {
-              x: 0,
-              y: 15
+              y: -10
             },
+            textFormat: formatLabel,
             textStyle: {
               fill: '#333',
               textAnchor: 'middle'
             }
-          },
-          lineStyle: {
-            fill: 'none',
-            stroke: '#e08080',
-            strokeDasharray: '4,2',
-            strokeWidth: 1
-          },
-          tickInterval: 2
+          };
         }
+      },
+
+      /**
+       * Option for style of data points.
+       *
+       * ```
+       * enable: If false, this option will be ignored.
+       * elements: Function or array of elements which define styles of data points. Currently, `circle` is the only permittable type.
+       *
+       * (ex) A point which consists of two different circles.
+       * elements: [
+       *   {
+       *     type: 'circle',
+       *     style: {
+       *       fill: '#fff',
+       *       r: '8',
+       *       stroke: '#e08080',
+       *       strokeWidth: 1.5
+       *     }
+       *   },
+       *   {
+       *     type: 'circle',
+       *     style: {
+       *       fill: '#e08080',
+       *       r: '3.5',
+       *       stroke: 'none',
+       *       strokeWidth: 0
+       *     }
+       *    }
+       *  ]
+       *
+       * (ex) Can be a function which returns an array of elements
+       * elements: function(p, i) {
+       *   if (i === this._someIndex) {
+       *     return [
+       *       {
+       *         type: 'circle',
+       *         style: {
+       *           fill: 'red',
+       *           r: '8'
+       *         }
+       *       }
+       *     ];
+       *   }
+       *   else {
+       *     return [
+       *       {
+       *         type: 'circle',
+       *         style: {
+       *           fill: 'blue',
+       *           r: '6'
+       *         }
+       *       }
+       *     ];
+       *   }
+       * }.bind(this)
+       * ```
+       */
+      point: {
+        type: Object,
+        value: function() {
+          return {
+            enable: true,
+            elements: [
+              {
+                type: 'circle',
+                style: {
+                  fill: '#e08080',
+                  r: '3',
+                  stroke: 'none',
+                  strokeWidth: 2
+                }
+              }
+            ]
+          };
+        },
+      },
+
+      /**
+       * Data rows
+       *
+       * ```
+       * (ex)
+       * [
+       *   [0, 0], [1, 1]
+       * ]
+       * ```
+       */
+      rows: {
+        type: Array
+      },
+
+      /**
+       * Option for x-axis grid lines and labels
+       */
+      xAxis: {
+        type: Object,
+        value: function() {
+          return {
+            enable: false,
+            label: {
+              offset: {
+                x: -4,
+                y: 5
+              },
+              textStyle: {
+                fill: '#333',
+                textAnchor: 'end'
+              }
+            },
+            lineStyle: {
+              fill: 'none',
+              stroke: '#e08080',
+              strokeDasharray: '4,2',
+              strokeWidth: 1
+            },
+            tickInterval: 2
+          }
+        }
+      },
+
+      /**
+       * X-axis range of chart area. If not specified, the range will be computed by data rows.
+       *
+       * ```
+       * min: X-axis value at the left (right if the origin is right) of chart area
+       * max: X-axis value at the right (left if the origin is right) of chart area
+       * ```
+       */
+      xRange: {
+        type: Object,
+        value: null
+      },
+
+      /**
+       * Option for y-axis grid lines and labels
+       */
+      yAxis: {
+        type: Object,
+        value: function() {
+          return {
+            enable: false,
+            label: {
+              offset: {
+                x: 0,
+                y: 15
+              },
+              textStyle: {
+                fill: '#333',
+                textAnchor: 'middle'
+              }
+            },
+            lineStyle: {
+              fill: 'none',
+              stroke: '#e08080',
+              strokeDasharray: '4,2',
+              strokeWidth: 1
+            },
+            tickInterval: 2
+          }
+        }
+      },
+
+      /**
+       * Y-axis range of chart area. If not specified, the range will be computed by data rows.
+       *
+       * ```
+       * min: Y-axis value at the bottom (top if the origin is top) of chart area
+       * max: Y-axis value at the top (bottom if the origin is top) of chart area
+       * ```
+       */
+      yRange: {
+        type: Object,
+        value: null
+      },
+
+      _transformer: {
+        type: Function,
+        computed: '_computeTransformer(origin, chartArea)'
       }
-    },
-
-    /**
-     * Y-axis range of chart area. If not specified, the range will be computed by data rows.
-     *
-     * ```
-     * min: Y-axis value at the bottom (top if the origin is top) of chart area
-     * max: Y-axis value at the top (bottom if the origin is top) of chart area
-     * ```
-     */
-    yRange: {
-      type: Object,
-      value: null
-    },
-
-    _transformer: {
-      type: Function,
-      computed: '_computeTransformer(origin, chartArea)'
     }
-  },
+  }
 
-  observers: [
-    '_paramsChanged(backgroundRectStyle, chartArea.*, origin, pathStyle, point, pointLabel.*, rows, xAxis, xRange, yAxis, yRange)'
-  ],
+  static get observers() {
+    return [
+      '_paramsChanged(backgroundRectStyle, chartArea.*, origin, pathStyle, point, pointLabel.*, rows, xAxis, xRange, yAxis, yRange)'
+    ]
+  }
 
-  _computeTransformer: function(origin, chartArea) {
+  _computeTransformer(origin, chartArea) {
     return getTransformer(origin, chartArea);
-  },
+  }
 
-  _draw: function(svg, pf) {
+  _draw(svg, pf) {
     var points = pf.points;
     var backgroundRectAttrs = toAttributes(this.backgroundRectStyle);
     var xAxisOpt = this.xAxis;
@@ -374,9 +379,9 @@ Polymer({
       }.bind(this));
     }
     return svg;
-  },
+  }
 
-  _drawBackgroundRect: function(rect, chartArea, attrs) {
+  _drawBackgroundRect(rect, chartArea, attrs) {
     rect.classList.add(BASE_CLASS + '-background-rect');
     rect.setAttribute('x', chartArea.left);
     rect.setAttribute('y', chartArea.top);
@@ -384,17 +389,17 @@ Polymer({
     rect.setAttribute('height', chartArea.height);
     setAttributes(rect, attrs);
     return rect;
-  },
+  }
 
-  _drawCircle: function(circle, point, attrs) {
+  _drawCircle(circle, point, attrs) {
     point = this._transformer.convertPoint(point);
     circle.setAttribute('cx', point.x);
     circle.setAttribute('cy', point.y);
     setAttributes(circle, attrs);
     return circle;
-  },
+  }
 
-  _drawXAxis: function(group, xAxisOpt, pf) {
+  _drawXAxis(group, xAxisOpt, pf) {
     var gridLines = document.createElementNS(NS, 'g');
     var gridLinesData = [];
     var gridLabels = document.createElementNS(NS, 'g');
@@ -446,9 +451,9 @@ Polymer({
     group.appendChild(gridLabels);
     this.fire('x-axis', {gridLines: gridLinesData}, NON_BUBBLES);
     return group;
-  },
+  }
 
-  _drawYAxis: function(group, yAxisOpt, pf) {
+  _drawYAxis(group, yAxisOpt, pf) {
     var gridLines = document.createElementNS(NS, 'g');
     var gridLinesData = [];
     var gridLabels = document.createElementNS(NS, 'g');
@@ -500,9 +505,9 @@ Polymer({
     group.appendChild(gridLabels);
     this.fire('y-axis', {gridLines: gridLinesData}, NON_BUBBLES);
     return group;
-  },
+  }
 
-  _drawPath: function(path, startPoint, moves, attrs) {
+  _drawPath(path, startPoint, moves, attrs) {
     var transformer = this._transformer;
     startPoint = transformer.convertPoint(startPoint);
     var d = moves.reduce(function(memo, vector) {
@@ -512,9 +517,9 @@ Polymer({
     path.setAttribute('d', d);
     setAttributes(path, attrs);
     return path;
-  },
+  }
 
-  _drawLabelAtAxis: function(text, point, labelOpt, pointValueName) {
+  _drawLabelAtAxis(text, point, labelOpt, pointValueName) {
     var option = labelOpt;
     var offset = option.offset || {};
     var attrs = toAttributes(option.textStyle);
@@ -525,9 +530,9 @@ Polymer({
     setAttributes(text, attrs);
     text.textContent = formatter(point[pointValueName]);
     return text;
-  },
+  }
 
-  _drawLabelAtPoint: function(text, point, index) {
+  _drawLabelAtPoint(text, point, index) {
     var option = this.pointLabel;
     var offset = option.offset || {};
     var row = this.rows[index];
@@ -539,9 +544,9 @@ Polymer({
     setAttributes(text, attrs);
     text.textContent = formatter(row, index);
     return text;
-  },
+  }
 
-  _paramsChanged: function(backgroundRectStyle, chartArea_, origin, pathStyle, point, pointLabel_, rows, xAxis_, xRange, yAxis_, yRange) {
+  _paramsChanged(backgroundRectStyle, chartArea_, origin, pathStyle, point, pointLabel_, rows, xAxis_, xRange, yAxis_, yRange) {
     this.debounce('draw', function() {
       var container = this.$.container;
       if (this._svg) {
@@ -593,7 +598,8 @@ Polymer({
    *
    * @event y-axis
    */
-});
+}
+window.customElements.define(NkLineChartElement.is, NkLineChartElement);
 
 function addClass(node, className) {
   node.classList.add(className);
